5f3d037df7b8f26f8d458c33aff1660d57cd3412
Max Wagner <max.wagner@productsup.com>
2025-08-17 23:21:59 +0200

    Refactor auth configuration to use environment variable for base URL instead of static value

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,14 +4,11 @@ import { betterAuth } from "better-auth";
 import { betterAuthComponent } from "../../convex/auth";
 import { type GenericCtx } from "../../convex/_generated/server";
 
-// You'll want to replace this with an environment variable
-const siteUrl = "http://localhost:3000";
-
 export const createAuth = (ctx: GenericCtx) =>
   // Configure your Better Auth instance here
   betterAuth({
     // All auth requests will be proxied through your TanStack Start server
-    baseURL: siteUrl,
+    baseURL: process.env.SITE_BASE_URL,
     database: convexAdapter(ctx, betterAuthComponent),
 
     socialProviders: {
